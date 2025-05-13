47791b6bc9898d86f869e46cd6997cc9e21b3e84
Akmaljon Abdirakhimov <akmaljonabdirakhimov@gmail.com>
2025-05-13 15:42:30 +0500

    duration range export

--- lib/index.dart
+++ lib/index.dart
@@ -7,3 +7,5 @@ export 'video_player_pip_platform_interface.dart' show VideoPlayerPipPlatform;
 // Export the PiP-enabled VideoPlayerController that supports view type selection
 export 'src/video_player_controller.dart';
 export 'src/extensions.dart';
+export 'package:video_player_platform_interface/video_player_platform_interface.dart'
+    show DurationRange;
